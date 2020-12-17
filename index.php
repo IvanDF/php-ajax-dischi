cb4f73a148b7e57fa4c5df57fa16f2bd08ea3fd6
Ivan <delfatti.ivan@gmail.com>
2020-12-17 14:52:59 +0100

    Ex done

--- index.php
+++ index.php
@@ -9,10 +9,34 @@
 </head>
 <body>
 
-    <div id="app">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laborum praesentium numquam dolorum maiores, ex repellendus error facere tempora aperiam odio excepturi qui quibusdam repellat iure sit mollitia nostrum a.</p>
-    </div>
-    
+    <?php // Item from database
+        include __DIR__ . '/partials/database.php' 
+    ?>
+<div id="app">
+    <header class="main-header">
+        <div class="container">
+            <img src="./img/logo.png" alt="Spotify">
+        </div>
+    </header>
+    <main class="main-section">
+        <div class="container">
+            <div class="songs">
+                <ul class="song-list">
+                    <?php foreach( $database as $song ) : ?>
+                        <li class="song-item txt-center">
+                            <img class="img-resp" src="<?php echo $song['poster'] ?>" alt="<?php echo $song['title'] ?>">
+                            <h3> <?php echo $song['title'] ?> </h3>
+                            <small> <?php echo $song['author'] ?> </small>
+                            <h3> <?php echo $song['year'] ?> </h3>
+                            <small> <?php echo $song['genre'] ?> </small>
+                        </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </div>
+    </main>
+</div>
+        
     <!-- MAIN JS -->
     <script src="./dist/js/main.js"></script>
 </body>
