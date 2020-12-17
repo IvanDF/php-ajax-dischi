9ec1298038f19831f7dd4ffed6ae3f92e706ce00
Ivan <delfatti.ivan@gmail.com>
2020-12-17 14:05:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- MAIN STYLE -->
+    <link rel="stylesheet" href="./dist/css/main.css">
+</head>
+<body>
+
+    <div id="app">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laborum praesentium numquam dolorum maiores, ex repellendus error facere tempora aperiam odio excepturi qui quibusdam repellat iure sit mollitia nostrum a.</p>
+    </div>
+    
+    <!-- MAIN JS -->
+    <script src="./dist/js/main.js"></script>
+</body>
+</html>
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// IMPORT VUE
+import Vue from 'vue';
+// IMPORT AXIOS
+import axios from 'axios'
+
+console.log('ciao');
+
+const app = new Vue ({
+    el: '#app',
+    data:{},
+    created() {
+        console.log('ciao Vue');
+    }
+});
